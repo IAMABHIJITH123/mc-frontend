804cfc51876cdc39fef9b11d429f750b0f2ef967
IAMABHIJITH123 <jijibnr@gmail.com>
2026-03-29 17:35:29 +0530

    Update api-client.ts

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.allorigins.win/raw?url=http://se-mlm-01.velrix.net:4819';
+const BASE_URL = '';
 
 export async function apiFetch<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
     const url = `${BASE_URL}${endpoint.startsWith('/') ? endpoint : `/${endpoint}`}`;
